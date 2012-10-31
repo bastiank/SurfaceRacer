d27884844f1495b3d1d06aadd000fdd87ea19f2d
Surface <surface@amilab-surface.(none)>
2012-10-31 10:41:16 +0100

    solved wiimote problems

--- start.sh
+++ start.sh
@@ -10,13 +10,20 @@ sleep 2
 /home/surface/Downloads/SurfaceRacer/application.linux32/SurfaceRacer > /dev/null &
 PP=$!
 
-/usr/bin/sudo wminput --config /home/surface/Downloads/SurfaceRacer/wiimote.conf --reconnect --wait &
+
+/usr/bin/sudo stdbuf -i 0 -o 0 -e 0 wminput --config /home/surface/Downloads/SurfaceRacer/wiimote.conf --reconnect --wait > /tmp/wminput.tmp &
 WPA=$!
 
-sleep 15
+while true; do
+        RESULT=`tail -n 1 /tmp/wminput.tmp`;
+        if [ "$RESULT" = "Ready." ]; then
+                /usr/bin/sudo wminput --config /home/surface/Downloads/SurfaceRacer/wiimote2.conf --reconnect --wait &
+                WPB=$!
+                break
+        fi
+        sleep 1
+done
 
-/usr/bin/sudo wminput --config /home/surface/Downloads/SurfaceRacer/wiimote2.conf --reconnect --wait &
-WPB=$!
 
 while true; do
 	if [ -f /proc/$PP/exe ] 
